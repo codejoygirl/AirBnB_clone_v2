49ca56675a75c3a946c32ebce7a40f34bac66a01
codejoygirl <joyegbu@gmail.com>
2024-04-19 00:49:44 -0700

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close(self):
+    """ Method to close the session """
     storage.close()
 
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port="5000")

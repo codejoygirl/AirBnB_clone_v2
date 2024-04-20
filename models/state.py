76662157eea040fa652cdd7225de5aa43b7ee288
Dev.Joy <Joyegbu@gmail.com>
2024-04-20 15:33:09 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
-import shlex
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
@@ -16,20 +15,15 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan",
+                          passive_deletes=True)
+
+    
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
